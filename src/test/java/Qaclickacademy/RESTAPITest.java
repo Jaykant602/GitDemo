90fe98215ad60fe6dcdb60e14f6122e518945d31
Jay <verma.jaykant@gmail.com>
2020-03-04 09:48:38 +0530

    first commit

---
+++ src/test/java/Qaclickacademy/RESTAPITest.java
@@ -0,0 +1,16 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+@Test
+public void postJira()
+{
+	System.out.println("postJira");
+}
+@Test
+public void deletetwitter()
+{
+	System.out.println("deletetwitter");
+}
+}
